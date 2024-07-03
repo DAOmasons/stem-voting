2bc179a57170e36d744dd4375a017f22305a715f
jordan <jordan.lesich@gmail.com>
2024-07-03 15:02:08 -0700

    rough in SBT token

---
+++ src/factories/gsRough/GSVoteToken.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract GSVotingToken is ERC20Votes, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address _holder)
+        ERC20(name, symbol)
+        ERC20Permit(name)
+    {
+        _mint(_holder, initialSupply);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+
+    function _transfer(address, address, uint256) internal pure override {
+        require(false, "SBT: Transfers are not allowed");
+    }
+
+    function transfer(address, uint256) public pure override returns (bool) {
+        require(false, "SBT: Transfers are not allowed");
+    }
+
+    function transferFrom(address, address, uint256) public pure override returns (bool) {
+        require(false, "SBT: Transfers are not allowed");
+    }
+}
