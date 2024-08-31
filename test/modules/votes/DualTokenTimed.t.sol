0fb7d9c946b4f19a3d5e2232358bc67c8a03993d
jordan <jordan.lesich@gmail.com>
2024-08-30 22:23:16 -0700

    test single votes

--- test/modules/votes/DualTokenTimed.t.sol
+++ test/modules/votes/DualTokenTimed.t.sol
@@ -17,6 +17,10 @@ import {BaalSetupLive} from "../../setup/BaalSetup.t.sol";
 contract DualTokenTimedV0Test is Test, ARBTokenSetupLive, BaalSetupLive, MockContestSetup, Accounts {
     event Initialized(address contest, uint256 duration, address daoToken, address contextToken);
     event VotingStarted(uint256 startTime, uint256 endTime, address pointModule);
+    event VoteCast(address indexed voter, bytes32 choiceId, uint256 amount, Metadata _reason, address _votingToken);
+    event VoteRetracted(
+        address indexed voter, bytes32 choiceId, uint256 amount, Metadata _reason, address _votingToken
+    );
 
     DualTokenTimedV0 votesModule;
     DualTokenPointsV0 pointsModule;
@@ -42,6 +46,7 @@ contract DualTokenTimedV0Test is Test, ARBTokenSetupLive, BaalSetupLive, MockCon
         vm.createSelectFork({blockNumber: startBlock, urlOrAlias: "arbitrumOne"});
         __setupArbToken();
         __setUpBaal();
+        __setupMockContest();
 
         votesModule = new DualTokenTimedV0();
         pointsModule = new DualTokenPointsV0();
@@ -51,6 +56,68 @@ contract DualTokenTimedV0Test is Test, ARBTokenSetupLive, BaalSetupLive, MockCon
         // Forge block.timestamp starts at 0
         // warp into the future so we can test
         vm.warp(INIT_TIME);
+
+        mockContest().cheatStatus(ContestStatus.Voting);
+    }
+
+    function test_setVotingTime_now() public {
+        _setVotingTime_now();
+
+        assertEq(votesModule.startTime(), block.timestamp);
+        assertEq(votesModule.endTime(), block.timestamp + TWO_WEEKS);
+        assertEq(uint8(mockContest().contestStatus()), uint8(ContestStatus.Voting));
+
+        assertEq(address(votesModule.pointModule()), address(pointsModule));
+    }
+
+    function test_setVotingTime_later() public {
+        _setVotingTime_later();
+
+        assertEq(votesModule.startTime(), block.timestamp + TWO_WEEKS);
+        assertEq(votesModule.endTime(), block.timestamp + TWO_WEEKS * 2);
+        assertEq(uint8(mockContest().contestStatus()), uint8(ContestStatus.Voting));
+
+        assertEq(address(votesModule.pointModule()), address(pointsModule));
+    }
+
+    function test_vote_daoToken() public {
+        _setVotingTime_now();
+        _vote_daoToken();
+
+        (uint256 daoVotes, uint256 contextVotes) = votesModule.getTotalVotesForChoices();
+
+        assertEq(daoVotes, _daoAmount);
+        assertEq(contextVotes, 0);
+        assertEq(votesModule.daoVotes(choice1(), address(voter1())), _daoAmount);
+        assertEq(votesModule.contextVotes(choice1(), address(voter1())), 0);
+    }
+
+    function test_vote_contextToken() public {
+        _setVotingTime_now();
+        _vote_contextToken();
+
+        (uint256 daoVotes, uint256 contextVotes) = votesModule.getTotalVotesForChoices();
+
+        assertEq(daoVotes, 0);
+        assertEq(contextVotes, _contextAmount);
+        assertEq(votesModule.daoVotes(choice1(), address(voter1())), 0);
+        assertEq(votesModule.contextVotes(choice1(), address(voter1())), _contextAmount);
+    }
+
+    function test_vote_all() public {
+        _setVotingTime_now();
+        _vote_daoToken();
+        _vote_contextToken();
+
+        (uint256 daoVotes, uint256 contextVotes) = votesModule.getTotalVotesForChoices();
+
+        votesModule.getTotalVotesForChoice(choice1());
+
+        assertEq(votesModule.daoVotes(choice1(), address(voter1())), _daoAmount);
+        assertEq(votesModule.contextVotes(choice1(), address(voter1())), _contextAmount);
+
+        assertEq(daoVotes, _daoAmount);
+        assertEq(contextVotes, _contextAmount);
     }
 
     //////////////////////////////
@@ -65,15 +132,42 @@ contract DualTokenTimedV0Test is Test, ARBTokenSetupLive, BaalSetupLive, MockCon
     // Helpers
     //////////////////////////////
 
+    function _vote_contextToken() private {
+        vm.expectEmit(true, false, false, true);
+        emit VoteCast(voter1(), choice1(), _contextAmount, _reason, address(loot()));
+
+        vm.startPrank(address(mockContest()));
+        votesModule.vote(voter1(), choice1(), _contextAmount, abi.encode(_reason, address(loot())));
+        vm.stopPrank();
+    }
+
+    function _vote_daoToken() private {
+        vm.expectEmit(true, false, false, true);
+        emit VoteCast(voter1(), choice1(), _daoAmount, _reason, address(arbToken()));
+
+        vm.startPrank(address(mockContest()));
+        votesModule.vote(voter1(), choice1(), _daoAmount, abi.encode(_reason, address(arbToken())));
+        vm.stopPrank();
+    }
+
+    function _setVotingTime_later() private {
+        _inititalize();
+
+        uint256 startTime = block.timestamp + TWO_WEEKS;
+
+        vm.expectEmit(true, false, false, true);
+        emit VotingStarted(startTime, startTime + TWO_WEEKS, address(pointsModule));
+
+        votesModule.setupVoting(startTime, address(pointsModule));
+    }
+
     function _setVotingTime_now() private {
         _inititalize();
 
-        mockContest().cheatStatus(ContestStatus.Voting);
-
         vm.expectEmit(true, false, false, true);
-        emit VotingStarted(block.timestamp, block.timestamp + TWO_WEEKS, address(0));
+        emit VotingStarted(block.timestamp, block.timestamp + TWO_WEEKS, address(pointsModule));
 
-        votesModule.setupVoting(0, address(0));
+        votesModule.setupVoting(0, address(pointsModule));
     }
 
     function _inititalize() private {
