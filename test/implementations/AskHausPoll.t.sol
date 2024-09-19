7bf741a8285d9735f1439a49f731c5066fadf73f
jordan <jordan.lesich@gmail.com>
2024-09-19 01:43:53 -0700

    ask haus base setup

---
+++ test/implementations/AskHausPoll.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {AskHausSetupLive} from "./../setup/AskHausSetup.t.sol";
+
+contract AskHausPollTest is Test, AskHausSetupLive {
+    function setUp() public {
+        vm.createSelectFork({blockNumber: START_BLOCK, urlOrAlias: "sepolia"});
+        __setupAskHausPoll();
+    }
+
+    function test() public {}
+}
---
+++ test/setup/AskHausSetup.t.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ContestStatus} from "../../src/core/ContestStatus.sol";
+import {BaalPointsV0} from "../../src/modules/points/BaalPoints.sol";
+import {Prepop} from "../../src/modules/choices/Prepop.sol";
+import {TimedVotes} from "../../src/modules/votes/TimedVotes.sol";
+import {EmptyExecution} from "../../src/modules/execution/EmptyExecution.sol";
+import {FastFactory} from "../../src/factories/gsRough/FastFactory.sol";
+import {Metadata} from "../../src/core/Metadata.sol";
+import {BaalGateV0} from "../../src/modules/choices/BaalGate.sol";
+import {BaalSetupLive} from "./BaalSetup.t.sol";
+import {Contest} from "../../src/Contest.sol";
+import {Accounts} from "./Accounts.t.sol";
+
+contract AskHausSetupLive is BaalSetupLive, Accounts {
+    event ContestInitialized(
+        address votesModule,
+        address pointsModule,
+        address choicesModule,
+        address executionModule,
+        bool isContinuous,
+        bool isRetractable,
+        ContestStatus status
+    );
+
+    address[] _voters;
+    uint256 constant SHARE_AMOUNT = 1_000e18;
+    uint256 constant LOOT_AMOUNT = 2_000e18;
+
+    uint256 constant START_BLOCK = 6668489;
+    uint256 START_TIMESTAMP = block.timestamp;
+    uint256 DELEGATE_TIMESTAMP = START_TIMESTAMP + 10;
+    uint256 SNAPSHOT_BLOCK = DELEGATE_TIMESTAMP + 15;
+    uint256 VOTE_BLOCK = DELEGATE_TIMESTAMP + 20;
+    uint256 constant TWO_WEEKS = 1209600;
+
+    BaalPointsV0 _baalPoints;
+    BaalGateV0 _baalChoices;
+    Prepop _prepopChoices;
+    Contest _contest;
+    TimedVotes _timedVotesModule;
+    EmptyExecution _executionModule;
+    FastFactory _factory;
+
+    Metadata _mockMetadata = Metadata(1, "qm....");
+
+    function __setupAskHausPoll() internal {
+        // ensure fork block number is synchronized with test environment block number
+        // assertEq(block.timestamp, START_TIMESTAMP);
+        __setUpBaalWithNewToken();
+        _setupVoters();
+        _fastFactorySetup();
+    }
+
+    function __setupAskHausContest() internal {}
+
+    function __setupAskHausSignalSession() internal {}
+
+    function _setupVoters() internal {
+        // vm.warp(DELEGATE_TIMESTAMP);
+        _voters = new address[](5);
+
+        _voters[0] = voter0();
+        _voters[1] = voter1();
+        _voters[2] = voter2();
+        _voters[3] = voter3();
+        _voters[4] = voter4();
+
+        uint256[] memory balances = new uint256[](5);
+
+        balances[0] = SHARE_AMOUNT;
+        balances[1] = SHARE_AMOUNT;
+        balances[2] = SHARE_AMOUNT;
+        balances[3] = SHARE_AMOUNT;
+        balances[4] = SHARE_AMOUNT;
+
+        address avatar = dao().avatar();
+
+        vm.startPrank(avatar);
+        dao().mintShares(_voters, balances);
+
+        balances = new uint256[](5);
+
+        balances[0] = LOOT_AMOUNT;
+        balances[1] = LOOT_AMOUNT;
+        balances[2] = LOOT_AMOUNT;
+        balances[3] = LOOT_AMOUNT;
+        balances[4] = LOOT_AMOUNT;
+
+        dao().mintLoot(_voters, balances);
+        vm.stopPrank();
+    }
+
+    function _fastFactorySetup() internal {
+        vm.startPrank(stemAdmin1());
+
+        _factory = new FastFactory(stemAdmin1());
+
+        factory().addAdmin(stemAdmin2());
+
+        Contest _contestImpl = new Contest();
+        BaalPointsV0 _baalPointsImpl = new BaalPointsV0();
+        BaalGateV0 _baalChoicesImpl = new BaalGateV0();
+        Prepop _prepopChoicesImpl = new Prepop();
+        TimedVotes _timedVotesModuleImpl = new TimedVotes();
+        EmptyExecution _executionModuleImpl = new EmptyExecution();
+
+        factory().setContestTemplate(_contestImpl.CONTEST_VERSION(), address(_contestImpl), _mockMetadata);
+        factory().setModuleTemplate(_baalPointsImpl.MODULE_NAME(), address(_baalPointsImpl), _mockMetadata);
+        factory().setModuleTemplate(_baalChoicesImpl.MODULE_NAME(), address(_baalChoicesImpl), _mockMetadata);
+        factory().setModuleTemplate(_prepopChoicesImpl.MODULE_NAME(), address(_prepopChoicesImpl), _mockMetadata);
+        factory().setModuleTemplate(_timedVotesModuleImpl.MODULE_NAME(), address(_timedVotesModule), _mockMetadata);
+        factory().setModuleTemplate(_executionModuleImpl.MODULE_NAME(), address(_executionModule), _mockMetadata);
+
+        vm.stopPrank();
+
+        assertTrue(factory().admins(stemAdmin1()));
+        assertTrue(factory().admins(stemAdmin2()));
+
+        assertEq(factory().contestTemplates(_contestImpl.CONTEST_VERSION()), address(_contestImpl));
+        assertEq(factory().moduleTemplates(_baalPointsImpl.MODULE_NAME()), address(_baalPointsImpl));
+        assertEq(factory().moduleTemplates(_baalChoicesImpl.MODULE_NAME()), address(_baalChoicesImpl));
+        assertEq(factory().moduleTemplates(_prepopChoicesImpl.MODULE_NAME()), address(_prepopChoicesImpl));
+        assertEq(factory().moduleTemplates(_timedVotesModuleImpl.MODULE_NAME()), address(_timedVotesModule));
+        assertEq(factory().moduleTemplates(_executionModuleImpl.MODULE_NAME()), address(_executionModule));
+    }
+
+    // function choicesModule() public view returns (HatsAllowList) {
+    //     return _choiceModule;
+    // }
+
+    // function pointsModule() public view returns (ERC20VotesPoints) {
+    //     return _pointsModule;
+    // }
+
+    // function votesModule() public view returns (TimedVotes) {
+    //     return _votesModule;
+    // }
+
+    // function executionModule() public view returns (EmptyExecution) {
+    //     return _executionModule;
+    // }
+
+    // function contest() public view returns (Contest) {
+    //     return _contest;
+    // }
+
+    function factory() public view returns (FastFactory) {
+        return _factory;
+    }
+}
