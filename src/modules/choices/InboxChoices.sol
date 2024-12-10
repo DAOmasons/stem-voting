c7006258cc6d034ed8e897c42f0afad5f2c42b20
jordan <jordan.lesich@gmail.com>
2024-12-09 21:30:43 -0800

    create choice list submodule for later execution

--- src/modules/choices/InboxChoices.sol
+++ src/modules/choices/InboxChoices.sol
@@ -15,9 +15,36 @@ abstract contract ChoiceCollector {
 
     bytes32[] public choiceIds;
 
-    function _registerChoice(bytes32 _choiceId, BasicChoice memory _choice) internal {}
+    function _registerChoice(bytes32 _choiceId, BasicChoice memory _choice) internal {
+        choices[_choiceId] = _choice;
+        choiceIds.push(_choiceId);
+    }
 
-    function _removeChoice(bytes32 _choiceId) internal {}
+    function _removeChoice(bytes32 _choiceId) internal {
+        require(choices[_choiceId].exists, "Choice does not exist");
+
+        bool found = false;
+        uint256 index;
+
+        // Find the index of the element to remove
+        for (uint256 i = 0; i < choiceIds.length; i++) {
+            if (choiceIds[i] == _choiceId) {
+                index = i;
+                found = true;
+                break;
+            }
+        }
+
+        require(found, "Choice not found");
+
+        // Swap with the last element and pop
+        if (index != choiceIds.length - 1) {
+            choiceIds[index] = choiceIds[choiceIds.length - 1];
+        }
+
+        choiceIds.pop();
+        delete choices[_choiceId];
+    }
 }
 
 contract InboxChoices is ChoiceCollector, IChoices, Initializable {
