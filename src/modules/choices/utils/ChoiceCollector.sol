209f3091f5b25f2d8dd8e1a7b873ff89f9e8a477
jordan <jordan.lesich@gmail.com>
2024-12-10 23:29:12 -0800

    natspec for conditional Allocator and ChoiceCollector

--- src/modules/choices/utils/ChoiceCollector.sol
+++ src/modules/choices/utils/ChoiceCollector.sol
@@ -4,15 +4,22 @@ pragma solidity ^0.8.24;
 import {BasicChoice} from "../../../core/Choice.sol";
 
 abstract contract ChoiceCollector {
+    ///@notice Mapping of choiceIds to BasicChoice
     mapping(bytes32 => BasicChoice) public choices;
 
+    ///@notice Array of choiceIds
     bytes32[] public choiceIds;
 
+    /// @notice Registers a choice with to the mapping and list
+    /// @param _choiceId The unique identifier for the choice
+    /// @param _choice The choice
     function _registerChoice(bytes32 _choiceId, BasicChoice memory _choice) internal {
         choices[_choiceId] = _choice;
         choiceIds.push(_choiceId);
     }
 
+    /// @notice Removes a choice from the mapping and list
+    /// @param _choiceId The unique identifier for the choice
     function _removeChoice(bytes32 _choiceId) internal {
         require(choices[_choiceId].exists, "Choice does not exist");
 
--- src/modules/points/utils/ConditionalAllocator.sol
+++ src/modules/points/utils/ConditionalAllocator.sol
@@ -2,16 +2,25 @@
 pragma solidity ^0.8.24;
 
 abstract contract ConditionalAllocator {
+    /// @notice mapping of voter to points
+    /// @dev voter => points
     mapping(address => uint256) public points;
 
+    /// @notice whether to accumulate points
     bool public shouldAccumulate;
 
+    /// @notice Allocates points to the user
+    /// @param _voter who is allocating their points
+    /// @param _amount of points allocated
     function _allocatePoints(address _voter, uint256 _amount) internal {
         if (shouldAccumulate) {
             points[_voter] += _amount;
         }
     }
 
+    /// @notice Releases points for the user
+    /// @param _voter who is releasing their points
+    /// @param _amount of points released
     function _releasePoints(address _voter, uint256 _amount) internal {
         if (shouldAccumulate) {
             require(points[_voter] >= _amount, "Not enough points");
