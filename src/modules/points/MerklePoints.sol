f3cb4089e354df0ae21123fcc437e6d4503f922b
jordan <jordan.lesich@gmail.com>
2024-12-13 00:10:12 -0800

    finish rough Merkle Points contract

--- src/modules/points/MerklePoints.sol
+++ src/modules/points/MerklePoints.sol
@@ -7,6 +7,10 @@ import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProo
 import {ModuleType} from "../../core/ModuleType.sol";
 
 contract MerklePoints is IPoints, Initializable {
+    /// @notice Emitted once the points module is initialized
+    event Initialized(address contest, bytes32 merkleRoot);
+
+    /// @notice The root of the merkle tree
     bytes32 public merkleRoot;
 
     /// @notice The name and version of the module
@@ -36,24 +40,64 @@ contract MerklePoints is IPoints, Initializable {
 
         contest = _contest;
         merkleRoot = _merkleRoot;
+
+        emit Initialized(_contest, _merkleRoot);
     }
 
     function claimPoints(address, bytes memory) external {}
 
-    function allocatePoints(address _voter, uint256 _amount, bytes memory _data) external onlyContest {}
+    /// @notice Allocates points to a user to track the amount voted
+    /// @param _voter The address of the user
+    /// @param _amount The amount of points to allocate
+    /// @param _data contains proof and user total vote amount
+    function allocatePoints(address _voter, uint256 _amount, bytes memory _data) external onlyContest {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(hasVotingPoints(_voter, _amount, _data), "Insufficient points available");
 
-    function releasePoints(address _voter, uint256 _amount, bytes memory _data) external onlyContest {}
+        allocatedPoints[_voter] += _amount;
+
+        emit PointsAllocated(_voter, _amount);
+    }
+
+    /// @notice Releases points from a user
+    /// @param _voter The address of the user
+    /// @param _amount The amount of points to release
+    function releasePoints(address _voter, uint256 _amount, bytes memory) external onlyContest {
+        require(_amount > 0, "Amount must be greater than 0");
+
+        // If this contract is not going to be used with the contest contract, use this check
+        // require(allocatedPoints[_voter] >= _amount, "Insufficient points allocated");
+
+        allocatedPoints[_voter] -= _amount;
+
+        emit PointsReleased(_voter, _amount);
+    }
 
     ///@notice Verifies if a user has the claimed points using OpenZeppelin's MerkleProof
     ///@param _user The address of the user
     ///@param _points The number of points claimed
     ///@param _proof The Merkle proof as an array of bytes32
-    function verifyPoints(address _user, uint256 _points, bytes32[] calldata _proof) public view returns (bool) {
+    function verifyPoints(address _user, uint256 _points, bytes32[] memory _proof) public view returns (bool) {
         bytes32 leaf = keccak256(abi.encodePacked(_user, _points));
         return MerkleProof.verify(_proof, leaf, merkleRoot);
     }
 
-    function hasVotingPoints(address _voter, uint256 _amount, bytes memory _data) external view returns (bool) {}
+    /// @notice Checks if a user has the specified voting points
+    /// @param _voter The address of the user
+    /// @param _amount The amount of points to check
+    /// @param _data contains proof and user total vote amount
+    function hasVotingPoints(address _voter, uint256 _amount, bytes memory _data) public view returns (bool) {
+        (bytes32[] memory _proof, uint256 _totalUserVotes) = abi.decode(_data, (bytes32[], uint256));
 
-    function hasAllocatedPoints(address _voter, uint256 _amount, bytes memory _data) external view returns (bool) {}
+        require(verifyPoints(_voter, _totalUserVotes, _proof), "User input data does not match merkle proof");
+
+        return _totalUserVotes - allocatedPoints[_voter] >= _amount;
+    }
+
+    /// @notice Checks if a user has allocated the specified amount
+    /// @param _voter The address of the user
+    /// @param _amount The amount of points to check
+    function hasAllocatedPoints(address _voter, uint256 _amount, bytes memory) external view returns (bool) {
+        return allocatedPoints[_voter] >= _amount;
+    }
 }
