33b1f37b2ae66d06bc282b159ee1989d49d5660c
jordan <jordan.lesich@gmail.com>
2024-12-20 23:44:36 -0800

    unit tests for Merkle Points

--- test/modules/points/MerklePoints.t.sol
+++ test/modules/points/MerklePoints.t.sol
@@ -9,6 +9,8 @@ import {MerkleSetup} from "../../setup/MerkleSetup.sol";
 import {Metadata} from "../../../src/core/Metadata.sol";
 
 contract MerklePointsTest is Test, MerkleSetup {
+    error InvalidInitialization();
+
     MerklePoints public merklePoints;
     address public contest;
     bytes32 public merkleRoot = 0xdc56428925fb0d14495de2f5d126f91282b8e6e69811397cf5b9f7e07f759902;
@@ -106,16 +108,71 @@ contract MerklePointsTest is Test, MerkleSetup {
     // Reverts
     //////////////////////////////
 
+    function testRevert_init_twice() public {
+        _initialize();
+        vm.expectRevert(InvalidInitialization.selector);
+        _initialize();
+    }
+
+    function testRevert_voteTooMuch() public {
+        _initialize();
+        _allocatePoints(0);
+        vm.expectRevert("Insufficient points available");
+        merklePoints.allocatePoints(voters[0], 1, abi.encode(abi.encode(_reason), abi.encode(proofs[0], 1e18)));
+    }
+
+    function testRevert_voteTwice() public {
+        _initialize();
+        _allocatePoints(0);
+        vm.expectRevert("Insufficient points available");
+        merklePoints.allocatePoints(voters[0], 1e18, abi.encode(abi.encode(_reason), abi.encode(proofs[0], 1e18)));
+    }
+
+    function testRevert_releaseZero() public {
+        _initialize();
+        _allocatePoints(0);
+        vm.expectRevert("Amount must be greater than 0");
+        merklePoints.releasePoints(voters[0], 0, "");
+    }
+
+    function testRevert_allocateZero() public {
+        _initialize();
+        vm.expectRevert("Amount must be greater than 0");
+        merklePoints.allocatePoints(voters[0], 0, "");
+    }
+
+    function testRevert_onlyContest() public {
+        vm.expectRevert("Only contest");
+        vm.prank(someGuy());
+        merklePoints.allocatePoints(voters[0], 1e18, "");
+
+        vm.expectRevert("Only contest");
+        vm.prank(someGuy());
+        merklePoints.releasePoints(voters[0], 1e18, "");
+    }
+
+    function testRevert_overMerkleAllocation() public {
+        _initialize();
+        vm.expectRevert("Insufficient points available");
+        merklePoints.allocatePoints(voters[0], 1e18 + 1, abi.encode(abi.encode(_reason), abi.encode(proofs[0], 1e18)));
+    }
+
     //////////////////////////////
     // Adversarial
     //////////////////////////////
+    function test_revert_notOnListAndStealProof() public {
+        _initialize();
+
+        vm.expectRevert("User input data does not match merkle proof");
+        merklePoints.allocatePoints(someGuy(), 1e18, abi.encode(abi.encode(_reason), abi.encode(proofs[0], 1e18)));
+    }
 
     // try to vote with more voting power than you actually have.
     function testRevert_spoofAmount() public {
         _initialize();
 
         vm.expectRevert("User input data does not match merkle proof");
-        merklePoints.allocatePoints(voters[0], 2e18, abi.encode(abi.encode(_reason), abi.encode(proofs[0], 2e18)));
+        merklePoints.allocatePoints(voters[0], 2e18, abi.encode(abi.encode(_reason), abi.encode(proofs, 2e18)));
     }
 
     //////////////////////////////
@@ -127,6 +184,33 @@ contract MerklePointsTest is Test, MerkleSetup {
         bool isValid = _verify();
 
         assertTrue(isValid);
+    }
+
+    function testHasVotingPoints() public {
+        _initialize();
+        bool hasPoints =
+            merklePoints.hasVotingPoints(voters[0], 1e18, abi.encode(abi.encode(_reason), abi.encode(proofs[0], 1e18)));
+
+        assertTrue(hasPoints);
+
+        vm.expectRevert("User input data does not match merkle proof");
+
+        hasPoints = merklePoints.hasVotingPoints(
+            voters[0], 1e18 + 1, abi.encode(abi.encode(_reason), abi.encode(proofs[0], 1e18 + 1))
+        );
+    }
+
+    function testHasAllocatedPoints() public {
+        _initialize();
+        bool hasPoints = merklePoints.hasAllocatedPoints(voters[0], 1e18, "");
+
+        assertFalse(hasPoints);
+
+        _allocatePoints(0);
+
+        hasPoints = merklePoints.hasAllocatedPoints(voters[0], 1e18, "");
+
+        assertTrue(hasPoints);
     }
 
     //////////////////////////////
